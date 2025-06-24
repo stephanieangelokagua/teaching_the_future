7a4229ab3e6c1cdca717b0b050945f3f4480a0e2
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2025-06-24 15:21:32 +0100

    restructured dependencies

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+export const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      id: 2,
+      name: 'Learning',
+      href: '#learning',
+    },
+    {
+      id: 3,
+      name: 'flash cards',
+      href: '#flashcards',
+    },
+    {
+      id: 4,
+      name: 'Reviews',
+      href: '#reviews',
+    },
+    {
+      id: 5,
+      name: 'Work',
+      href: '#work',
+    },
+    {
+      id: 6,
+      name: 'Contact',
+      href: '#contact',
+    },
+  ];
+
+
+  // export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  //   return {
+  //     deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+  //     deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+  //     cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+  //     reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+  //     ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+  //     targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  //   };
+  // };
